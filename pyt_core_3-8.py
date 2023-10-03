ca06aaa9beed50447fb6135252daf86949554cbb
VitalyMarty <vitalymarty@gmail.com>
2023-10-03 13:15:24 +0300

    10.03.23

--- pyt_core_3-8.py
+++
@@ -1,7 +0,0 @@
-def cost_delivery(quantity, *args, discount-0):
-    if discount is None:
-        return 5 + (quantity -1) * 2
-    else:
-        return 5 + (quantity -1) * 2 * discount
-    
-print(cost_delivery)
